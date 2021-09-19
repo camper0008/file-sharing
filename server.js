db3ccefb70354f75e619a0aae9b128be0898e107
piet <42157469+camper0008@users.noreply.github.com>
2021-09-19 22:12:55 +0200

    clear button and removed hacky solutions

--- server.js
+++ server.js
@@ -8,22 +8,36 @@ app.use('/', express.static(__dirname + '/user_files'))
 app.use(fileUpload());
 
 const fs = require('fs');
+const path = require('path');
 
 app.get('/', (req, res) => {
     fs.readdir('./user_files', (err, data) => {
         if (err) throw err;
-        let dataString = ""
+        let dataString = ''
         for (fileIndex in data) {
-            if (data[fileIndex] !== '.gitkeep') dataString += `<li><a href="${data[fileIndex]}">${data[fileIndex]}</a></li>`
+            if (data[fileIndex] !== '.gitkeep') dataString += `<li><a href='${data[fileIndex]}'>${data[fileIndex]}</a></li>`
         }
-        res.send(`
+        res.send(/*html*/`
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang='en'>
+        <head>
+            <style>
+                input, ul {
+                    font-size: 1.05rem;
+                    font-family: Arial, sans-serif;
+                }
+            </style>
+        </head>
         <body>
             <h1>Upload files</h1>
-            <form enctype="multipart/form-data" action="/upload" method="POST">
-                <input name="files" type="file" multiple><br>
-                <input name="submit" type="submit" value="Upload">
+            <form enctype='multipart/form-data' action='/upload' method='POST'>
+                <input name='files' type='file' multiple>
+                <br><br>
+                <input name='submit' type='submit' value='Upload'>
+            </form>
+            <br>
+            <form enctype='multipart/form-data' action='/clear' method='POST'>
+                <input name='submit' type='submit' value='Clear Files'>
             </form>
             <br>
             <ul>
@@ -38,14 +52,26 @@ app.get('/', (req, res) => {
 app.post('/upload', (req, res) => {
     if (req.files.files.mv) {
         let file = req.files.files
-        file.mv(__dirname + "/user_files/" + file.name);
+        file.mv(path.join(__dirname, '/user_files/', file.name));
     } else if (req.files) {
         let files = req.files.files
         for (file in files) {
-            files[file].mv(__dirname + "/user_files/" + files[file].name);
+            files[file].mv(path.join(__dirname, '/user_files/', files[file].name));
         }
     }
-    res.send(`<script>window.location.replace('/')</script>`)
+    res.redirect('/');
+})
+app.post('/clear', (req, res) => {
+
+    fs.readdir('./user_files', (err, data) => {
+        if (err) throw err;
+        for (fileIndex in data) {
+            if (data[fileIndex] !== '.gitkeep') fs.unlink(path.join(__dirname, '/user_files/', data[fileIndex]), (err) => {
+                if (err) throw err;
+            });
+        }
+    });
+    res.redirect('/');
 })
 
 app.listen(PORT, () => {
