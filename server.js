c6de4fbc566d4766f6823ffbcca765e4227e80f7
piet <spetakel123@gmail.com>
2021-12-17 11:30:37 +0100

    public folder

--- server.js
+++ server.js
@@ -1,79 +1,67 @@
+const PORT = 3500;
+const USER_FILES = [];
+
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const app = express();
-
-const PORT = 3500;
-
-app.use('/', express.static(__dirname + '/user_files'))
-app.use(fileUpload());
-
 const fs = require('fs');
 const path = require('path');
 
-app.get('/', (req, res) => {
-    fs.readdir('./user_files', (err, data) => {
-        if (err) throw err;
-        let dataString = ''
-        for (fileIndex in data) {
-            if (data[fileIndex] !== '.gitkeep') dataString += `<li><a href='${data[fileIndex]}'>${data[fileIndex]}</a></li>`
-        }
-        res.send(/*html*/`
-        <!DOCTYPE html>
-        <html lang='en'>
-        <head>
-            <style>
-                input, ul {
-                    font-size: 1.05rem;
-                    font-family: Arial, sans-serif;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Upload files</h1>
-            <form enctype='multipart/form-data' action='/upload' method='POST'>
-                <input name='files' type='file' multiple>
-                <br><br>
-                <input name='submit' type='submit' value='Upload'>
-            </form>
-            <br>
-            <form enctype='multipart/form-data' action='/clear' method='POST'>
-                <input name='submit' type='submit' value='Clear Files'>
-            </form>
-            <br>
-            <ul>
-                ${dataString}
-            </ul>
-        </body>
-        </html>`
-        )
-    })
-})
 
-app.post('/upload', (req, res) => {
-    if (req.files.files.mv) {
-        let file = req.files.files
-        file.mv(path.join(__dirname, '/user_files/', file.name));
-    } else if (req.files) {
-        let files = req.files.files
-        for (file in files) {
-            files[file].mv(path.join(__dirname, '/user_files/', files[file].name));
-        }
-    }
-    res.redirect('/');
-})
-app.post('/clear', (req, res) => {
+const main = () => {
+    const app = express();
+    app.use(fileUpload());
+    app.use('/', express.static(path.join(__dirname, '/public')));
 
-    fs.readdir('./user_files', (err, data) => {
-        if (err) throw err;
-        for (fileIndex in data) {
-            if (data[fileIndex] !== '.gitkeep') fs.unlink(path.join(__dirname, '/user_files/', data[fileIndex]), (err) => {
-                if (err) throw err;
-            });
-        }
+    app.get('/filelist', (req, res) => {
+        const filenames = [];
+        for (let i = 0; i < USER_FILES.length; i++)
+            filenames.push(USER_FILES[i].name);
+        res.json({files: filenames})
     });
-    res.redirect('/');
-})
 
-app.listen(PORT, () => {
-    console.log('Server started on port', PORT)
-})
+    app.get('/files/:name', (req, res) => {
+        const fileName = req.params["name"]
+        for (let i = 0; i < USER_FILES.length; i++) {
+            if (fileName === USER_FILES[i].name) {
+                res.status(200).send(USER_FILES[i].data);
+                return
+            }
+        }
+        res.status(404).json({msg: "file not found"});
+    });
+
+    app.post('/upload', (req, res) => {
+        if (req.files === null)
+            res.status(400).redirect('/');
+        if (req.files.files.mv) {
+            const file = req.files.files
+            USER_FILES.push({
+                name: file.name,
+                data: file.data,
+                mimetype: file.mimetype,
+            })
+        } else if (req.files) {
+            const files = req.files.files
+            for (let i in files) {
+                const file = files[i];
+                USER_FILES.push({
+                    name: file.name,
+                    data: file.data,
+                    mimetype: file.mimetype,
+                })
+            }
+        }
+        res.status(200).redirect('/');
+    })
+
+    app.post('/clear', (req, res) => {
+        USER_FILES.splice(0, USER_FILES.length);
+        res.status(200).json({msg: "files cleared successfully"})
+    })
+
+    app.listen(PORT, () => {
+        console.log('Server started on port', PORT)
+    })
+}
+
+main();
